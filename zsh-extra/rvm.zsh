f33b0b47fb1df6b8d4fdbe9b7c93b896b39601fb
Vitor Pellegrino <vitorp@gmail.com>
2012-02-07 11:38:19 -0200

    Tweaking Ruby vm

--- zsh-extra/rvm.zsh
+++ zsh-extra/rvm.zsh
@@ -5,3 +5,11 @@ function rvm_prompt_info() {
 }
 
 [[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
+
+# Ruby VM performance tweaks
+# https://gist.github.com/1688857
+export RUBY_HEAP_MIN_SLOTS=1000000
+export RUBY_HEAP_SLOTS_INCREMENT=1000000
+export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
+export RUBY_GC_MALLOC_LIMIT=1000000000
+export RUBY_HEAP_FREE_MIN=500000
