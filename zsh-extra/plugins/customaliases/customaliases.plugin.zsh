e9779e74d0b894fc5809884b699cc939525aeecd
Vitor Pellegrino <vitorp@gmail.com>
2012-02-06 23:55:37 -0200

    Creating custom aliases file if that doesn't exist already

--- zsh-extra/plugins/customaliases/customaliases.plugin.zsh
+++ zsh-extra/plugins/customaliases/customaliases.plugin.zsh
@@ -2,6 +2,13 @@ function add-alias() {
   name=$1 
   value=$2
 
+  custom_aliases_file="$ZSH/custom-aliases.zsh"
+
+  # Create custom_aliases_file it if it doesn't exist
+  if [[ ! -f $custom_aliases_file ]]; then
+    touch $custom_aliases_file
+  fi
+
   if [[ -z $name ]]; then
     echo "Invalid alias name, please provide a name for your alias."
     return 1
@@ -12,7 +19,7 @@ function add-alias() {
     return 1
   fi
 
-  echo "alias $name='$value'" >> $ZSH/custom-aliases.zsh
+  echo "alias $name='$value'" >> $custom_aliases_file
   eval "alias $name='$value'"
   alias $name
 
