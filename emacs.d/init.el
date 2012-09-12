3d624b4454edfe6b503b00d10f99ee61cc035247
Vitor Pellegrino <pellegrino@soundcloud.com>
2012-09-12 12:08:11 +0200

    Loading twilight theme correctly

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -21,7 +21,7 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-ruby starter-kit-js starter-kit-bindings)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-ruby starter-kit-js starter-kit-bindings twilight-theme)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
@@ -33,3 +33,20 @@
 (setq auto-save-file-name-transforms
                 `((".*" ,temporary-file-directory t)))
 
+
+;; Remove trailing whitespaces automatically
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("6cfe5b2f818c7b52723f3e121d1157cf9d95ed8923dbc1b47f392da80ef7495d" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;; Looks nice for me
+(load-theme 'twilight)
